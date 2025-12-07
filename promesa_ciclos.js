664aab7b1fd423eb90d92035f5cc6627c3fd0379
Serranomanuel <Serranomanuel106@gmail.com>
2025-12-06 20:05:22 -0500

    promesas con ciclos actualizados

--- promesa_ciclos.js
+++ promesa_ciclos.js
@@ -1,8 +1,26 @@
 function promesas(n){
     let respuesta = [];
     for (let i = 0; i < n ; i++) {
-        respuesta.push(new Promise((resolve, reject) => {resolve("ok")}));
+        if (i % 2 == 0){
+            respuesta.push(new Promise((reject, resolver) =>{
+                reject("Rechazada");
+            }))
+        }else{
+            respuesta.push(new Promise((resolver, reject) => {
+                resolver("Ok")
+            }))
+        }
     }
     return respuesta; 
 }
 
+let respuestas = promesas(9);
+
+Promise.all(respuesta)
+    .then((a) => {
+        console.log(a);
+    })
+    .catch((e) => {
+        console.log(e);
+        
+    })
